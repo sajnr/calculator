83fa443dcd7fc4031c2a94a5e764a138648fdad4
sajnr <isaac20mut@gmail.com>
2023-08-02 15:40:37 +0300

    update the calculator.js

--- calculator.js
+++ calculator.js
@@ -1,96 +1,95 @@
 // DECLARE VARS
 let numStr = ""; //operand a
-let prevNum = null; //operand b
+let prevNum = null; // operand b
 let selectedOperator = null;
 
-// SELECTED ELEMENTS
+// SELECT ELEMENTS,
 const functionBtns = document.querySelectorAll(
-    ".calculator__btns > .calculator__btn"
-  );
-  const clearBtn = document.getElementById("clear-btn");
-const deleteBtn = document.getElementById("delete-btn");
+  ".calculator__btns > .calculator__btn"
+);
+const clearBtn = document.getElementById("clear-btn");
 const topScreenText = document.getElementById("calculator-screen-top");
 const bottomScreenText = document.getElementById("calculator-screen-bottom");
 
 // DEFINE FNS
 function sum(a, b) {
-    return a + b;
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate() {
+  if (selectedOperator.includes("+")) {
+    return Math.round(sum(prevNum, parseFloat(numStr)) * 1000) / 1000;
+  } else if (selectedOperator.includes("-")) {
+    return Math.round(subtract(prevNum, parseFloat(numStr)) * 1000) / 1000;
+  } else if (selectedOperator.includes("x")) {
+    return Math.round(multiply(prevNum, parseFloat(numStr)) * 1000) / 1000;
+  } else if (selectedOperator.includes("÷")) {
+    if (numStr === "0" || numStr === "") {
+      return "Error: cannot divide by 0";
+    }
+    return Math.round(divide(prevNum, parseFloat(numStr)) * 1000) / 1000;
   }
-  function subtract(a, b) {
-    return a - b;
-  }
-  function multiply(a, b) {
-    return a * b;
-  }
-  function divide(a, b) {
-    return a / b;
-  }
-  
-  function selectedOperator() {
-    if (selectedOperator. includes ("+")) {
-        return Math.round(sum(PrevNum, parseFloat(numStr)) * 1000) / 1000;
-    }else if (selectedOperator.includes ("−")) {
-        return Math.round(substract(PrevNum, parseFloat(numStr)) * 1000) / 1000;
-    }else if  (selectedOperator.includes ("×")) {
-        return Math.round(multiply(PrevNum, parseFloat(numStr)) * 1000) / 1000;
-    }else if(selectedOperator.includes ("÷")) {
-        if (numStr === "0" || numStr === ""){
-          return "error. cannot divide by 0";
-        }
-        return Math.round(multiply(PrevNum, parseFloat(numStr)) * 1000) / 1000;
+}
+
+function onCalculatorfunctionBtnClick() {
+  let result = null;
+  let char = this.innerText;
+
+  if (/[0-9.]/.test(char)) {
+    if (char === "." && numStr.includes(".")) {
+      return;
+    }
+
+    numStr += char;
+    bottomScreenText.innerText = numStr;
+    if (prevNum !== null) {
+      topScreenText.innerText = `${prevNum} ${selectedOperator}`;
     }
   }
 
-  function onCalculatorfunctionBtnClick() {
-    let result = null;
-    let char = this.innerText;
-  
-    if (/[0-9.]/.test(char)) {
-      if (char === "." && numStr.includes(".")) {
-        return;
-      }
-  
-      numStr += char;
-      bottomScreenText.innerText = numStr;
-      if (prevNum !== null) {
-        topScreenText.innerText = `${prevNum} ${selectedOperator}`;
-      }
+  if (/[\+\-\x\÷]/.test(char)) {
+    // CASE ONE: if both operands not defined
+    if (prevNum === null && numStr !== "") {
+      selectedOperator = char;
+      prevNum = parseFloat(numStr);
+      numStr = "";
+      topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+      bottomScreenText.innerText = prevNum;
     }
-
-    if (/[\+\-\x\÷]/.test(char)) {
-        // CASE ONE: if both operands not defined
-        if (prevNum === null && numStr !== "") {
-          selectedOperator = char;
-          prevNum = parseFloat(numStr);
-          numStr = "";
-          topScreenText.innerText = `${prevNum} ${selectedOperator}`;
-          bottomScreenText.innerText = prevNum;
-        }
-        // CASE TWO: if both operands defined
-        else if (prevNum !== null && numStr !== "") {
-            result = operate();
+    // CASE TWO: if both operands defined
+    else if (prevNum !== null && numStr !== "") {
+      result = operate();
       prevNum = result;
       numStr = "";
       selectedOperator = char;
       topScreenText.innerText = `${result} ${selectedOperator}`;
       bottomScreenText.innerText = result;
     }
- // CASE THREE: if click on different operators in a row
- else if (prevNum !== null && numStr === "") {
-    selectedOperator = char;
-    topScreenText.innerText = `${prevNum} ${selectedOperator}`;
-    bottomScreenText.innerText = prevNum;
+    // CASE THREE: if click on different operators in a row
+    else if (prevNum !== null && numStr === "") {
+      selectedOperator = char;
+      topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+      bottomScreenText.innerText = prevNum;
+    }
+    // CASE FOUR: if operate with undefined operand b
+    else if (numStr === "") {
+      selectedOperator = char;
+      prevNum = 0;
+      topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+      bottomScreenText.innerText = prevNum;
+    }
   }
-  // CASE FOUR: if operate with undefined operand b
-  else if (numStr === "") {
-    selectedOperator = char;
-    prevNum = 0;
-    topScreenText.innerText = `${prevNum} ${selectedOperator}`;
-    bottomScreenText.innerText = prevNum;
-  }
-}
 
-if (char === "=") {
+  if (char === "=") {
     if (prevNum !== null && numStr !== "") {
       result = operate();
       topScreenText.innerText = `${prevNum} ${selectedOperator} ${numStr} =`;
@@ -104,16 +103,23 @@ if (char === "=") {
   }
 }
 
-function onclearbuttonclick() {
+function onClearButtonClick() {
+  numStr = "";
+  prevNum = null;
+  selectedOperator = null;
+  topScreenText.innerText = "";
+  bottomScreenText.innerText = "";
+}
+
+function ondDeleteButtonClick() {
     numStr = "";
     prevNum = null;
     selectedOperator = null;
     topScreenText.innerText = "";
     bottomScreenText.innerText = "";
-}
-
+  }
 // EVENT LISTENERS
 functionBtns.forEach((functionBtn) => {
-    functionBtn.addEventListener("click", onCalculatorfunctionBtnClick);
-  });
-  clearBtn.addEventListener("click", onClearButtonClick);
+  functionBtn.addEventListener("click", onCalculatorfunctionBtnClick);
+});
+clearBtn.addEventListener("click", onClearButtonClick);
