8b27b7e8a35cf8635b057908fbcab4cfeef8dea4
sajnr <isaac20mut@gmail.com>
2023-08-02 14:24:16 +0300

    update the calculator.js

--- calculator.js
+++ calculator.js
@@ -7,7 +7,6 @@ let selectedOperator = null;
 const functionBtns = document.querySelectorAll(
     ".calculator__btns > .calculator__btn"
   );
-
   const clearBtn = document.getElementById("clear-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const topScreenText = document.getElementById("calculator-screen-top");
@@ -29,12 +28,56 @@ function sum(a, b) {
   
   Function ongotpointercapture() {
     if (selectedOperator. includes ("+")) {
-        return Math.round(sum(PrevNum, parseFloat(numStr)) * 1000) / 1000
+        return Math.round(sum(PrevNum, parseFloat(numStr)) * 1000) / 1000;
     }else if (selectedOperator.includes ("−")) {
-        return Math.round(substract(PrevNum, parseFloat(numStr)) * 1000) / 1000
+        return Math.round(substract(PrevNum, parseFloat(numStr)) * 1000) / 1000;
     }else if  (selectedOperator.includes ("×")) {
-        return Math.round(multiply(PrevNum, parseFloat(numStr)) * 1000) / 1000
+        return Math.round(multiply(PrevNum, parseFloat(numStr)) * 1000) / 1000;
     }else if(selectedOperator.includes ("÷")) {
-        return Math.
+        if (numStr === "0" || numStr === ""){
+          return "error. cannot divide by 0";
+        }
+        return Math.round(multiply(PrevNum, parseFloat(numStr)) * 1000) / 1000;
     }
+  }
+
+  function onCalculatorfunctionBtnClick() {
+    let result = null;
+    let char = this.innerText;
+  
+    if (/[0-9.]/.test(char)) {
+      if (char === "." && numStr.includes(".")) {
+        return;
+      }
+  
+      numStr += char;
+      bottomScreenText.innerText = numStr;
+      if (prevNum !== null) {
+        topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+      }
+    }
+
+    if (/[\+\-\x\÷]/.test(char)) {
+        // CASE ONE: if both operands not defined
+        if (prevNum === null && numStr !== "") {
+          selectedOperator = char;
+          prevNum = parseFloat(numStr);
+          numStr = "";
+          topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+          bottomScreenText.innerText = prevNum;
+        }
+        // CASE TWO: if both operands defined
+        else if (prevNum !== null && numStr !== "") {
+            result = operate();
+      prevNum = result;
+      numStr = "";
+      selectedOperator = char;
+      topScreenText.innerText = `${result} ${selectedOperator}`;
+      bottomScreenText.innerText = result;
+    }
+ // CASE THREE: if click on different operators in a row
+ else if (prevNum !== null && numStr === "") {
+    selectedOperator = char;
+    topScreenText.innerText = `${prevNum} ${selectedOperator}`;
+    bottomScreenText.innerText = prevNum;
   }
